642277224d82391160667a010e611b7e2fc8dcb6
Patryk Obara <dreamer.tan@gmail.com>
2017-02-06 00:03:35 +0100

    Added bashrc files

--- plugins/files.bash
+++ plugins/files.bash
@@ -30,3 +30,33 @@ link_file ()
 		echo "- linking $target failed"
 	fi
 }
+
+force_link_file ()
+{
+	local object=$1
+	local target=$2
+
+	if [ ! -e "$object" ] ; then
+		echo "- $target not created ($object doesn't exist)"
+		return 1
+	fi
+
+	local link="$(readlink $target)"
+
+	if [ "$link" = "$object" ] ; then
+		echo "+ $target linked already"
+		return 0
+	fi
+
+	if [ -f "$target" ] ; then
+		rm $target
+	fi
+
+	mkdir -p $(dirname $target)
+
+	if ln -s "$object" "$target" ; then
+		echo "+ $target linked"
+	else
+		echo "- linking $target failed"
+	fi
+}
