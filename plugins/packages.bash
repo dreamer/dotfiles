e62f320d0d1e1d94c0cef732f1044036232ead09
Patryk Obara <dreamer.tan@gmail.com>
2017-02-05 16:16:11 +0100

    Added my old list of packages

---
+++ plugins/packages.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+list_file ()
+{
+	cat $1 | sed -e 's/\(#.*\)//'
+}
+
+install_packages ()
+{
+	if ! which dnf >/dev/null ; then
+		echo "- packages can't be installed (dnf missing)"
+		return 1
+	fi
+
+	if rpm -q $(list_file $1) > /dev/null ; then
+		echo "+ all packages installed already"
+		return 0
+	fi
+
+	if sudo dnf install -y $(list_file $1) 2> /dev/null ; then
+		echo "+ $1 packages installed"
+		rpm -q $(list_file $1) | grep -e "is not installed"
+		return 0
+	else
+		echo "- $1 packages not installed"
+		return 1
+	fi
+}
