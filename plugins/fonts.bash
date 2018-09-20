c8a1313cecd10afa26827e6b0cfb5e2efb602416
Patryk Obara <dreamer.tan@gmail.com>
2018-09-20 18:21:10 +0200

    Install Core Fonts

--- plugins/fonts.bash
+++ plugins/fonts.bash
@@ -14,6 +14,14 @@ download_fonts () {
 		wget -O "$file" "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
 	done
 
+	# Microsoft Core Fonts
+
+	local arial=~/.local/share/fonts/arial.ttf
+	if [ ! -f "$arial" ] ; then
+		winetricks corefonts
+		cp ~/.wine/drive_c/windows/Fonts/*.ttf ~/.local/share/fonts
+	fi
+
 	fc-cache -f
 
 	echo "+ fc-cache updated"
