b8445d37ebb3d26a7f8e06bd1fd19dad69736af1
Patryk Obara <dreamer.tan@gmail.com>
2021-04-12 13:47:42 +0200

    Update Tilix dconf

--- plugins/terminal.bash
+++ plugins/terminal.bash
@@ -2,6 +2,9 @@
 
 change_tilix_defaults ()
 {
+	# to store:
+	# dconf dump /com/gexperts/Tilix/ > dotfiles/tilix.dconf
+
 	dconf load /com/gexperts/Tilix/ < dotfiles/tilix.dconf
 
 	echo "+ tilix settings loaded"
