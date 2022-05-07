144c4c0d1380e6309790017753bb39dd62bdbffd
Patryk Obara <dreamer.tan@gmail.com>
2022-05-07 19:40:13 +0200

    Install FiraCode font from Fedora repos

--- plugins/fonts.bash
+++ plugins/fonts.bash
@@ -4,16 +4,6 @@ download_fonts () {
 
 	mkdir -p ~/.local/share/fonts
 
-	# FiraCode
-
-	for type in Bold Light Medium Regular Retina; do
-		local file=~/.local/share/fonts/FiraCode-${type}.ttf
-		if [ -f "$file" ] ; then
-			continue
-		fi
-		wget -O "$file" "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
-	done
-
 	# Microsoft Core Fonts
 
 	local arial=~/.local/share/fonts/arial.ttf
